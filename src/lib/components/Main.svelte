d8bddca0074a5ea794f7a682bb5e6e9c9c23540f
Wyrdix <wyrdix@gmail.com>
2026-05-16 22:39:47 +0200

    ✨ Add settings pane with animation step filter

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import { buildMaze, DefaultMazeImplementation, type Maze } from "$lib/maze";
-  import { Card } from "flowbite-svelte";
+  import {
+    Accordion,
+    AccordionItem,
+    Card,
+    Input,
+    Label,
+  } from "flowbite-svelte";
   import MazeViewer from "./GenericMazeViewer.svelte";
   import {
     Generator,
@@ -9,6 +15,12 @@
     type State,
   } from "$lib/generators/generation_by_sets";
   import { generate } from "$lib/generator";
+  import { Factory, FilePlay, Settings } from "lucide-svelte";
+  import type { TreeValue } from "./TreeBoolean.svelte";
+  import TreeBoolean from "./TreeBoolean.svelte";
+
+  let rows = $state(3);
+  let cols = $state(3);
 
   const gen = Generator({});
   const initial: DefaultMazeImplementation<
@@ -23,23 +35,117 @@
     gen,
     initial,
   ).map((v) => v.maze);
+
+  let animationsStepFilter: TreeValue<boolean> = $state([
+    false,
+    {
+      selection: false,
+      neighbour: false,
+      "random neighbour": false,
+      "randon neighbour entry": false,
+      "clear wall": false,
+    },
+  ]);
+
+  let animationsStepText: TreeValue<string> = [
+    "",
+    {
+      selection: "Cell selection",
+      neighbour: "Neighbour lookup",
+      "random neighbour": "Neighbour pick",
+      "randon neighbour entry": "Neighbour set lookup",
+      "clear wall": "Fusion",
+    },
+  ];
 </script>
 
-<Card class="relative w-full h-full max-w-none max-h-none flex flex-row">
-  <div class="flex items-center p-10 mx-auto">
-    <MazeViewer {mazes}>
-      {#snippet cellViewer(maze, pos, cell)}
-        <div
-          class="w-full h-full min-h-0 min-w-0 bg-gray-300 dark:bg-gray-700 rounded-md"
-        >
+<Card class="relative max-h-full w-full h-full max-w-none flex flex-row">
+  <!-- Left empty div -->
+  <div class="flex-1"></div>
+
+  <!-- Center MazeViewer -->
+  <div class="relative mx-auto flex-1">
+    <div class="flex items-center p-10 h-full w-full">
+      <MazeViewer {mazes}>
+        {#snippet cellViewer(maze, pos, cell)}
           <div
-            class="w-full h-full transition-all"
-            class:bg-blue-400={cell == "first"}
-            class:bg-red-400={cell == "second"}
-            class:bg-yellow-400={cell == "neighbour"}
-          ></div>
-        </div>
-      {/snippet}
-    </MazeViewer>
+            class="w-full h-full min-h-0 min-w-0 bg-gray-300 dark:bg-gray-700 rounded-md"
+          >
+            <div
+              class="w-full h-full transition-all"
+              class:bg-blue-400={cell == "first"}
+              class:bg-red-400={cell == "second"}
+              class:bg-yellow-400={cell == "neighbour"}
+            ></div>
+          </div>
+        {/snippet}
+      </MazeViewer>
+      <div
+        class="absolute w-full h-full left-0 backdrop-blur-sm flex justify-center items-center"
+      >
+        <h2 class="center inline text-xl">No data</h2>
+      </div>
+    </div>
+  </div>
+
+  <!-- Right Accordion -->
+  <div class="flex-1 overflow-hidden max-h-full">
+    <Accordion
+      flush
+      class="h-full overflow-y-auto **:outline-none **:select-none **:focus:ring-0 px-5"
+    >
+      <AccordionItem>
+        {#snippet header()}
+          <h3 class="**:inline *:align-text-bottom">
+            <Settings /> Settings
+          </h3>
+        {/snippet}
+        <Accordion
+          flush
+          multiple
+          class="h-full **:outline-none **:select-none **:focus:ring-0 px-5"
+        >
+          <AccordionItem>
+            {#snippet header()}
+              <h3 class="**:inline *:align-text-bottom">
+                <Factory /> Generator
+              </h3>
+            {/snippet}
+            <form class="flex flex-col gap-5 **:[appearance:textfield]">
+              <Label for="row">Rows:</Label>
+              <Input
+                type="number"
+                id="row"
+                aria-describedby="helper-text-explanation"
+                placeholder="3"
+                required
+                bind:value={rows}
+              />
+              <Label for="col">Cols:</Label>
+              <Input
+                type="number"
+                id="col"
+                aria-describedby="helper-text-explanation"
+                placeholder="90210"
+                required
+                bind:value={cols}
+              />
+            </form>
+          </AccordionItem>
+          <AccordionItem>
+            {#snippet header()}
+              <h3 class="**:inline *:align-text-bottom">
+                <FilePlay /> Animation
+              </h3>
+            {/snippet}
+
+            <TreeBoolean
+              bind:filter={animationsStepFilter}
+              name={animationsStepText}
+            />
+          </AccordionItem>
+        </Accordion>
+      </AccordionItem>
+    </Accordion>
   </div>
 </Card>
---
+++ src/lib/components/TreeBoolean.svelte
@@ -0,0 +1,125 @@
+<script lang="ts" module>
+  export type TreeValue<T> = T | [T, { [key: string]: TreeValue<T> }];
+
+  type TreeInternalNodeValue<T> = [T, { [key: string]: TreeValue<T> }];
+
+  function deepReplace<T>(root: T, oldV: unknown, newV: unknown): T {
+    if (root === oldV) return newV as T;
+    if (typeof root !== "object") return root;
+    return Object.fromEntries(
+      Object.entries(root as {}).map(([k, v]): [string, any] => [
+        k,
+        deepReplace(v, oldV, newV),
+      ]),
+    ) as T;
+  }
+
+  function deepSearch(root: any, value: unknown): boolean {
+    if (root === value) return true;
+    if (typeof root !== "object") return false;
+    return Object.values(root as {}).find((v) => deepSearch(v, value)) != null;
+  }
+</script>
+
+<script lang="ts">
+  import { Accordion, AccordionItem, Checkbox, Input } from "flowbite-svelte";
+  import StepFilter from "./TreeBoolean.svelte";
+
+  let {
+    name,
+    filter = $bindable(),
+    parentIsGroup = false,
+  }: {
+    name: TreeValue<string>;
+    filter: TreeValue<boolean>;
+    parentIsGroup?: boolean;
+  } = $props();
+</script>
+
+{#if typeof name == "string" || typeof filter == "boolean"}
+  <Checkbox
+    inline
+    bind:checked={filter as boolean}
+    onmousedown={(e) => e.stopPropagation()}
+  >
+    {name}</Checkbox
+  >
+{:else}
+  <Accordion flush>
+    {#each Object.keys(filter[1]) as key}
+      {@const name_value = name[1][key]}
+      {@const filter_value = filter[1][key]}
+
+      {#if typeof name_value == "string" || typeof filter_value == "boolean"}
+        <div
+          class="flex items-center justify-between w-full font-medium text-left py-4 my-1 text-gray-500 dark:text-gray-400"
+        >
+          <StepFilter
+            name={name_value}
+            bind:filter={
+              () => filter_value,
+              (newFilter) => {
+                let parentNewFilter: TreeValue<boolean> = [
+                  (filter as TreeInternalNodeValue<boolean>)[0],
+                  { ...(filter as TreeInternalNodeValue<boolean>)[1] },
+                ];
+                parentNewFilter[1][key] = newFilter;
+                filter = parentNewFilter;
+              }
+            }
+            {parentIsGroup}
+          />
+        </div>
+      {:else}
+        <AccordionItem>
+          {#snippet header()}
+            <StepFilter
+              name={name_value[0]}
+              bind:filter={
+                () => filter_value[0],
+                (newFilter) => {
+                  let parentNewFilter: TreeValue<boolean> = [
+                    (filter as TreeInternalNodeValue<boolean>)[0],
+                    { ...(filter as TreeInternalNodeValue<boolean>)[1] },
+                  ];
+                  parentNewFilter[1][key] = [
+                    newFilter,
+                    parentIsGroup
+                      ? deepReplace(
+                          filter_value[1],
+                          filter_value[0],
+                          newFilter as boolean,
+                        )
+                      : filter_value[1],
+                  ];
+
+                  filter = parentNewFilter;
+                }
+              }
+              {parentIsGroup}
+            />
+          {/snippet}
+          <StepFilter
+            name={name_value}
+            bind:filter={
+              () => filter_value,
+              (newFilter) => {
+                let parentNewFilter: TreeValue<boolean> = [
+                  (filter as TreeInternalNodeValue<boolean>)[0],
+                  { ...(filter as TreeInternalNodeValue<boolean>)[1] },
+                ];
+                parentNewFilter[1][key] = [
+                  deepSearch(newFilter, true),
+                  newFilter[1],
+                ];
+                console.log(parentNewFilter);
+                filter = parentNewFilter;
+              }
+            }
+            {parentIsGroup}
+          />
+        </AccordionItem>
+      {/if}
+    {/each}
+  </Accordion>
+{/if}
