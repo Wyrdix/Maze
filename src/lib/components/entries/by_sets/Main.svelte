d8c99087cf8cc7029715203445e8e9b4814be3f1
Wyrdix <wyrdix@gmail.com>
2026-05-19 13:41:34 +0200

    ✨ Add an algorithm picker on the main page

--- src/lib/components/entries/by_sets/Main.svelte
+++ src/lib/components/entries/by_sets/Main.svelte
@@ -69,112 +69,107 @@
   );
 </script>
 
-<Card class="relative max-h-full w-full h-full max-w-none flex flex-row">
-  <!-- Left empty div -->
-  <div class="flex-1"></div>
-
-  <!-- Center MazeViewer -->
-  <div class="relative mx-auto flex-1">
-    <div class="flex items-center p-10 h-full w-full">
-      <MazeViewer mazes={filtered_mazes.map((v) => v.maze)}>
-        {#snippet cellViewer(maze, pos, cell)}
-          <div
-            class="w-full h-full min-h-0 min-w-0 bg-gray-300 dark:bg-gray-700 rounded-md"
-          >
-            <div
-              class="w-full h-full transition-all"
-              class:bg-blue-400={cell == "first"}
-              class:bg-red-400={cell == "second"}
-              class:bg-yellow-400={cell == "neighbour"}
-            ></div>
-          </div>
-        {/snippet}
-      </MazeViewer>
-      {#if mazes.length == 0}
+<!-- Center MazeViewer -->
+<div class="relative mx-auto flex-1">
+  <div class="flex items-center p-10 h-full w-full">
+    <MazeViewer mazes={filtered_mazes.map((v) => v.maze)}>
+      {#snippet cellViewer(maze, pos, cell)}
         <div
-          class="absolute w-full h-full left-0 backdrop-blur-sm flex justify-center items-center"
+          class="w-full h-full min-h-0 min-w-0 bg-gray-300 dark:bg-gray-700 rounded-md"
         >
-          <h2 class="center inline text-xl">No data</h2>
+          <div
+            class="w-full h-full transition-all"
+            class:bg-blue-400={cell == "first"}
+            class:bg-red-400={cell == "second"}
+            class:bg-yellow-400={cell == "neighbour"}
+          ></div>
         </div>
-      {/if}
-    </div>
+      {/snippet}
+    </MazeViewer>
+    {#if mazes.length == 0}
+      <div
+        class="absolute w-full h-full left-0 backdrop-blur-sm flex justify-center items-center"
+      >
+        <h2 class="center inline text-xl">No data</h2>
+      </div>
+    {/if}
   </div>
+</div>
 
-  <!-- Right Accordion -->
-  <div class="flex-1 overflow-hidden max-h-full flex flex-col">
-    <Button
-      class="m-2 flex flex-row gap-5"
-      color="indigo"
-      disabled={generating}
-      onclick={() => generateMaze()}
-      >Generate <RefreshCcw
-        class={`${generating ? "animate-spin" : ""}`}
-      /></Button
-    >
-    <Accordion
-      flush
-      class="h-full overflow-y-auto **:outline-none **:select-none **:focus:ring-0 px-5"
-    >
-      <AccordionItem>
-        {#snippet header()}
-          <h3 class="**:inline *:align-text-bottom">
-            <Settings /> Settings
-          </h3>
-        {/snippet}
-        <Accordion
-          flush
-          class="h-full **:outline-none **:select-none **:focus:ring-0 px-5"
-        >
-          <AccordionItem>
-            {#snippet header()}
-              <h3 class="**:inline *:align-text-bottom">
-                <Factory /> Generator
-              </h3>
-            {/snippet}
-            <form class="flex flex-col gap-5 **:[appearance:textfield]">
-              <Label for="row">Rows:</Label>
-              <Input
-                type="number"
-                id="row"
-                aria-describedby="helper-text-explanation"
-                placeholder="3"
-                required
-                bind:value={rows}
-              />
-              <Label for="col">Cols:</Label>
-              <Input
-                type="number"
-                id="col"
-                aria-describedby="helper-text-explanation"
-                placeholder="90210"
-                required
-                bind:value={columns}
-              />
-            </form>
-          </AccordionItem>
-          <AccordionItem>
-            {#snippet header()}
-              <h3 class="**:inline *:align-text-bottom">
-                <FilePlay /> Animation
-              </h3>
-            {/snippet}
-
-            <TreeBoolean
-              bind:filter={animationsStepFilter}
-              name={[
-                "",
-                {
-                  selection: "Cell selection",
-                  neighbour: "Neighbour lookup",
-                  "random neighbour": "Neighbour pick",
-                  "randon neighbour entry": "Neighbour set lookup",
-                  "clear wall": "Fusion",
-                },
-              ]}
+<!-- Right Accordion -->
+<div class="flex-1 overflow-hidden max-h-full flex flex-col">
+  <Button
+    class="m-2 flex flex-row gap-5"
+    color="indigo"
+    disabled={generating}
+    onclick={() => generateMaze()}
+    >Generate <RefreshCcw
+      class={`${generating ? "animate-spin" : ""}`}
+    /></Button
+  >
+  <Accordion
+    flush
+    class="h-full overflow-y-auto **:outline-none **:select-none **:focus:ring-0 px-5"
+  >
+    <AccordionItem>
+      {#snippet header()}
+        <h3 class="**:inline *:align-text-bottom">
+          <Settings /> Settings
+        </h3>
+      {/snippet}
+      <Accordion
+        flush
+        class="h-full **:outline-none **:select-none **:focus:ring-0 px-5"
+      >
+        <AccordionItem>
+          {#snippet header()}
+            <h3 class="**:inline *:align-text-bottom">
+              <Factory /> Generator
+            </h3>
+          {/snippet}
+          <form class="flex flex-col gap-5 **:[appearance:textfield]">
+            <Label for="row">Rows:</Label>
+            <Input
+              type="number"
+              id="row"
+              aria-describedby="helper-text-explanation"
+              placeholder="3"
+              required
+              bind:value={rows}
             />
-          </AccordionItem>
-        </Accordion>
-      </AccordionItem>
-    </Accordion>
-  </div>
-</Card>
+            <Label for="col">Cols:</Label>
+            <Input
+              type="number"
+              id="col"
+              aria-describedby="helper-text-explanation"
+              placeholder="90210"
+              required
+              bind:value={columns}
+            />
+          </form>
+        </AccordionItem>
+        <AccordionItem>
+          {#snippet header()}
+            <h3 class="**:inline *:align-text-bottom">
+              <FilePlay /> Animation
+            </h3>
+          {/snippet}
+
+          <TreeBoolean
+            bind:filter={animationsStepFilter}
+            name={[
+              "",
+              {
+                selection: "Cell selection",
+                neighbour: "Neighbour lookup",
+                "random neighbour": "Neighbour pick",
+                "randon neighbour entry": "Neighbour set lookup",
+                "clear wall": "Fusion",
+              },
+            ]}
+          />
+        </AccordionItem>
+      </Accordion>
+    </AccordionItem>
+  </Accordion>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,56 @@
-<script>
-  import Main from "$lib/components/entries/by_tree/Main.svelte";
+<script lang="ts">
+  import MainByTree from "$lib/components/entries/by_tree/Main.svelte";
+  import MainBySets from "$lib/components/entries/by_sets/Main.svelte";
+  import { Button, Card } from "flowbite-svelte";
+
+  let choice: "by_tree" | "by_sets" = $state("by_tree");
 </script>
 
 <div class="relative h-full max-w-none w-auto flex-1 m-10">
-  <Main />
+  <Card class="relative max-h-full w-full h-full max-w-none flex flex-row">
+    <!-- Left empty div -->
+    <div class="flex-1 flex-col p-5">
+      <Card size="lg" class="p-4 text-center sm:p-8 md:p-10">
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          By tree
+        </h5>
+        <p
+          class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400"
+        >
+          Generate a maze using a randomized depth first search, the root is
+          picked randomly.
+        </p>
+        <Button
+          class="bg-brand"
+          onclick={() => (choice = "by_tree")}
+          disabled={choice == "by_tree"}>Select</Button
+        >
+      </Card>
+      <Card size="lg" class="p-4 text-center sm:p-8 md:p-10">
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          By sets
+        </h5>
+        <p
+          class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400"
+        >
+          Generate a maze using a randomized union of sets, each cells starting
+          it its own set.
+        </p>
+        <Button
+          class="bg-brand"
+          onclick={() => (choice = "by_sets")}
+          disabled={choice == "by_sets"}>Select</Button
+        >
+      </Card>
+    </div>
+    {#if choice === "by_tree"}
+      <MainByTree />
+    {:else}
+      <MainBySets />
+    {/if}
+  </Card>
 </div>
